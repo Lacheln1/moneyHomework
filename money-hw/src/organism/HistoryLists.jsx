c60f34f298f2a962e536a67359b17ada5d41021a
Lacheln <psionict@naver.com>
2025-04-25 15:44:01 +0900

    거래내역 리스트 구조 제작

--- money-hw/src/organism/HistoryLists.jsx
+++ money-hw/src/organism/HistoryLists.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import css from "./HistoryLists.module.css";
-const HistoryLists = () => {
-  return <div></div>;
+const HistoryLists = ({ data }) => {
+  return (
+    <div className={css.listContainer}>
+      <span className={css.title}>4월용돈</span>
+      <span className={css.price}>+300,000</span>
+      <div className={css.bar}></div>
+    </div>
+  );
 };
 
 export default HistoryLists;
